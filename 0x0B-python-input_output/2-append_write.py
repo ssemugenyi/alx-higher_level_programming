8b48a0f7f96ba282aa53273edd17369efc396603
ssemugenyi <ssemugenyiisaac2@gmail.com>
2023-02-21 00:44:13 -0800

    Fixed

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,20 +1,9 @@
 #!/usr/bin/python3
-"""read_lines
-"""
-import os
+"""The append text function container"""
 
 
-def read_lines(filename="", nb_lines=0):
-    """Takes in str filename to read, and n lines w/ int nb_lines
-    """
-
-    with open(filename, encoding="utf-8") as readFile:
-        lineNum = 0
-        while True:
-            line = readFile.readline()
-            lineNum += 1
-            print(line, end='')
-            if lineNum >= nb_lines and nb_lines > 0:
-                break
-            if not line:
-                break
+def append_write(filename="", text=""):
+    """appends a string and returns the number of characters added"""
+    with open(filename, "a", encoding="utf-8") as file:
+        lines = file.write(text)
+        return lines
