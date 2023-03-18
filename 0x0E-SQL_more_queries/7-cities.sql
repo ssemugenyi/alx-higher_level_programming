3e1be1a8d8df37bb100793cfe723d8091890c722
ssemugenyi <ssemugenyiisaac2@gmail.com>
2023-03-18 10:06:32 -0700

    Fixed

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -8,8 +8,8 @@ USE hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS cities(
     id INT AUTO_INCREMENT,
-    states_id INT NOT NULL,
+    state_id INT NOT NULL,
     name VARCHAR(256),
     PRIMARY KEY (id),
-    FOREIGN KEY (state_id) REFERENCES states(id) ON DELETE CAScADE
+    FOREIGN KEY (state_id) REFERENCES states(id) ON DELETE CASCADE
 );
